267456abc8a49de198b58ffe12a4547b77010846
Viktorija Krivokapić <72516782+ViktorijaKrivokapic@users.noreply.github.com>
2024-12-19 14:41:51 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Add adds two integers and returns the result.
+func Add(a, b int) int {
+    return a + b
+}
+
+func main() {
+    fmt.Println("Hello, Travis CI!")
+}
